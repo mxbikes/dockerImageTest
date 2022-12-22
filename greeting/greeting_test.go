7db126f80a0c96c3d82c3477c805a78b7e700e95
RikVanHaaren <contact@rikvanhaaren.com>
2022-12-22 14:09:32 +0100

    example

--- greeting/greeting_test.go
+++ greeting/greeting_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreet(t *testing.T) {
 	got := Greet("Wonka")
-	want := "Hello, Wonka!"
+	want := "Hello, Wonka"
 
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
